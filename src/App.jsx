b0f8ecb48130dd6a0e8328b2fa4640990600aa9a
Devperegrine <149635505+BELSOFT-SYSTEMS@users.noreply.github.com>
2023-12-29 17:52:59 +0100

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,29 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
-
+import { Component } from "react";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Testimonials from "./components/Testimonials/Testimonials";
+import News from "./components/News/News";
 import Banner from "./components/Banner/Banner";
-import Blogs from "./components/Blogs/Blogs";
-import Swipe from "./components/Blogs/Swipe";
 import Footer from "./components/Footer/Footer";
-import Hero from "./components/Hero.jsx/Hero";
-// import Navbar from "./components/navbar/Navbar";
-import Navbar2 from "./components/navbar/Navbar2";
-import { useEffect } from "react";
-import Service from "./components/Service/Service";
-import BannerDetails from "./components/BannerDetails/BannerDetails";
-import Banner1 from "./assets/blog1.jpg";
-import Banner2 from "./assets/blog3.jpg";
-
-function App() {
-  useEffect(() => {
-    AOS.init({
-      offset: 100,
-      duration: 500,
-      easing: "ease-in-sine",
-      delay: 100,
-    });
-    AOS.refresh();
-  }, []);
-  return (
-    <>
-      <div className="dark:bg-slate-900 dark:text-white">
-        <div className="fixed left-0 right-0 top-0 z-50 bg-gradient-to-l from-violet-900 via-violet-800 to-violet-900 ">
-          {/* <Navbar /> */}
-          <Navbar2 />
-        </div>
-        <Hero />
-        <Service />
-        <BannerDetails reverse={true} img={Banner1} />
-        <BannerDetails img={Banner2} />
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Nav />
+        <About />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <News />
         <Banner />
-        <Blogs />
-        {/* <Swipe /> */}
         <Footer />
       </div>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
