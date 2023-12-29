787c4248fcd3b89e617feceed91ffede3058c0b0
Devperegrine <149635505+BELSOFT-SYSTEMS@users.noreply.github.com>
2023-12-29 17:40:22 +0100

    last commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,80 +1,115 @@
-import { Component } from "react";
-import { CgInstagram } from "react-icons/cg";
-import { AiFillTwitterSquare } from "react-icons/ai";
-import { AiFillFacebook } from "react-icons/ai";
-import "./style.css";
-class Footer extends Component {
-  render() {
-    return (
-      <footer id="contact" className="container footer__container">
-        <div className="about">
-          <h3>LOGO</h3>
-          <p className="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste saepe
-            voluptatum repellat placeat aperiam. Molestiae consectetur molestias
-            magni laudantium doloremque!
-          </p>
-          <ul className="footer__socials">
-            <li>
-              <a href="#">
-                <AiFillFacebook />
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="rounded-t-3xl bg-gradient-to-r from-violet-950 to-violet-900">
+      <section className="mx-auto max-w-[1200px] text-white">
+        <div className=" grid py-5 md:grid-cols-3">
+          <div className=" px-4 py-8 ">
+            <h1 className="mb-3 text-justify text-xl font-bold sm:text-left sm:text-3xl">
+              <a href="/#home" className="">
+                COZ
+                <span className="inline-block font-bold text-primary">WEB</span>
               </a>
-            </li>
-            <li>
-              <a href="#">
-                <CgInstagram />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <AiFillTwitterSquare />
-              </a>
-            </li>
-          </ul>
+            </h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Possimus, voluptate.{" "}
+            </p>
+            <br />
+            <div className="flex items-center gap-3">
+              <FaLocationArrow />
+              <p>Noida, Uttar Pradesh</p>
+            </div>
+            <div className="mt-3 flex items-center gap-3">
+              <FaMobileAlt />
+              <p>+91 123456789</p>
+            </div>
+            {/* Social Handle */}
+          </div>
+          <div className="col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10 ">
+            <div className="">
+              <div className="px-4 py-8 ">
+                <h1 className="mb-3 text-justify text-xl font-bold sm:text-left sm:text-xl">
+                  Important Links
+                </h1>
+                <ul className={`flex flex-col gap-3`}>
+                  <li className="cursor-pointer transition-all duration-300 hover:translate-x-[2px]">
+                    Home
+                  </li>
+                  <li className="cursor-pointer transition-all duration-300 hover:translate-x-[2px]">
+                    About
+                  </li>
+                  <li className="cursor-pointer transition-all duration-300 hover:translate-x-[2px]">
+                    Services
+                  </li>
+                  <li className="cursor-pointer transition-all duration-300 hover:translate-x-[2px]">
+                    Login
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              <div className="px-4 py-8 ">
+                <h1 className="mb-3 text-justify text-xl font-bold sm:text-left sm:text-xl">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  <li className="cursor-pointer transition-all duration-300 hover:translate-x-[2px]">
+                    Privacy Policy
+                  </li>
+                  <li className="cursor-pointer transition-all duration-300 hover:translate-x-[2px]">
+                    Services
+                  </li>
+                  <li className="cursor-pointer transition-all duration-300 hover:translate-x-[2px]">
+                    About us
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              <div className="px-4 py-8 ">
+                <h1 className="mb-3 text-justify text-xl font-bold sm:text-left sm:text-xl">
+                  Social Links
+                </h1>
+                <div className="flex flex-col gap-3">
+                  <h1>Subscribe to our newsletter</h1>
+                  <input
+                    className="rounded-full px-3 py-1 text-black focus:border-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-500 "
+                    type="text"
+                    placeholder="Email"
+                  />
+                  <div className="mt-6 flex items-center gap-3">
+                    <a href="#" className="duration-200 hover:scale-105">
+                      <FaInstagram className="text-3xl" />
+                    </a>
+                    <a href="#" className="duration-200 hover:scale-105">
+                      <FaFacebook className="text-3xl" />
+                    </a>
+                    <a href="#" className="duration-200 hover:scale-105">
+                      <FaLinkedin className="text-3xl" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="services">
-          <h3>Services</h3>
-          <ul>
-            <li>
-              <a href="#" className="text">
-                Cyber Security
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text">
-                IT COnsultant
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text">
-                Strategy & Research
-              </a>
-            </li>
-          </ul>
+        <div>
+          <div className="border-t-2 border-gray-300/50 py-6 text-center">
+            @copyright 2024 Travery || Dilshad
+          </div>
         </div>
-        <div className="contact">
-          <h3>Contact</h3>
-          <ul>
-            <li>
-              <address className="text">
-                Office: 232 California Road Imperial
-              </address>
-            </li>
-            <li>
-              <a href="tel:+212698584458" className="text">
-                Tel : +212698584458
-              </a>
-            </li>
-            <li>
-              <a href="mailto:lormail@gmail.com" className="text">
-                Email: lormail@gmail.com
-              </a>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    );
-  }
-}
+      </section>
+    </div>
+  );
+};
 
 export default Footer;
--- src/components/Nav/Quote.jsx
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-import Button from "../Global__Components/Button";
-class Quote extends Component {
-  render() {
-    return <Button content="Get a Quote" />;
-  }
-}
-export default Quote;
