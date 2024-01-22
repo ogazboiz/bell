eb2f594038ee4091c12275ad825f416e07bae53d
Bayomi Aremo <8955yomi@gmail.com>
2024-01-21 21:02:37 -0700

    all team members added ✔ except one ⁉

--- src/components/MeetTheteam/Meettheteam.jsx
+++ src/components/MeetTheteam/Meettheteam.jsx
@@ -7,6 +7,12 @@ import BelbaImage from './images/Belba-Ngoy-CEO_Founder.jpeg'
 import Abaleke from './images/Abaleke-Chiedozie-Digital-Marketing-Expert.jpg'
 import Abu from './images/Abu David-Fullstack-Developer.jpg'
 import chidi from './images/Chidi Nkwocha-Web Developer.jpg'
+import Daniel from './images/Daniel-Atang-Johnson-Company Secretary.jpeg'
+import Folaranmi from './images/Folaranmi-Israel-FrontEndDeveloper.jpg'
+import Godwin from './images/Godwi-Gabriel-Ogazi-Graphics-Designer.jpg'
+import taofeek from './images/Taofeek Ajibola Oluwatoba-Product-Designer.jpg'
+import victory from './images/Okuobeya Victory Ogheneochuko-Social-Media-Manager_Content-writer.jpg'
+// import Queensley from './images/Queensley-Belba-Ngoy-CFO_Director-Marketing&Finance.JPG'
 import './style.css'
 const teamMembers = [
   { name: "Belba Ngoy", role: "Ceo/founder", email: "Belsoftsystems@gmail.com", image: BelbaImage },
@@ -15,6 +21,12 @@ const teamMembers = [
   { name: "Abaleke Chiedozie", role: "Digital Marketing", email: "Belsoftsystems@gmail.com", image: Abaleke },
   { name: "Abu David", role: "Full Stack developer", email: "Belsoftsystems@gmail.com", image: Abu },
   { name: "Chidi Nkwocha", role: "Web-developer", email: "Belsoftsystems@gmail.com", image: chidi },
+  { name: "Daniel Atang", role: "Secretary", email: "Belsoftsystems@gmail.com", image: Daniel },
+  { name: "Folaranmi Isreal", role: "Front-End-Dev", email: "Belsoftsystems@gmail.com", image: Folaranmi },
+  { name: "Godwin Ogazi", role: "Graphics-Designer", email: "Belsoftsystems@gmail.com", image: Godwin },
+  { name: "Okuobeya Victory", role: "social-Media-Manager", email: "Belsoftsystems@gmail.com", image: victory},
+  { name: "Taofeek Ajibola", role: "Product-Designer", email: "Belsoftsystems@gmail.com", image: taofeek },
+  // { name: "Queensly Belba Ngoy", role: "Marketing&Finance", email: "Belsoftsystems@gmail.com", image: Queensley},
   
 ];
 
