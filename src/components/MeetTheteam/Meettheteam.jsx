990301ecb9d8a77587c400d1699cf8ab034d0322
Bayomi Aremo <8955yomi@gmail.com>
2024-01-21 20:44:13 -0700

    added team members

--- src/components/MeetTheteam/Meettheteam.jsx
+++ src/components/MeetTheteam/Meettheteam.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Meettheteam.css";
 import Teammembercard from "./Teammembercard";
-import testImage from './test.jpeg';
-import princeImage from './prince.png';
-
-
+import testImage from './images/test.jpeg';
+import princeImage from './images/prince.png';
+import BelbaImage from './images/Belba-Ngoy-CEO_Founder.jpeg'
+import Abaleke from './images/Abaleke-Chiedozie-Digital-Marketing-Expert.jpg'
+import Abu from './images/Abu David-Fullstack-Developer.jpg'
+import chidi from './images/Chidi Nkwocha-Web Developer.jpg'
+import './style.css'
 const teamMembers = [
+  { name: "Belba Ngoy", role: "Ceo/founder", email: "Belsoftsystems@gmail.com", image: BelbaImage },
   { name: "Abayomi Aremo", role: "Frontend Developer", email: "abayomiaremo0@gmail.com", image: testImage },
   { name: "Akpolo Prince", role: "Frontend Developer", email: "princeakpolo@gmail.com", image: princeImage },
+  { name: "Abaleke Chiedozie", role: "Digital Marketing", email: "Belsoftsystems@gmail.com", image: Abaleke },
+  { name: "Abu David", role: "Full Stack developer", email: "Belsoftsystems@gmail.com", image: Abu },
+  { name: "Chidi Nkwocha", role: "Web-developer", email: "Belsoftsystems@gmail.com", image: chidi },
+  
 ];
 
 const Meettheteam = () => {
@@ -17,7 +25,7 @@ const Meettheteam = () => {
         <h2>
           <a href="#services">Meet The Team</a>
         </h2>
-        <div>
+        <div className="team-card-container">
           {teamMembers.map((member, index) => (
             <Teammembercard
               key={index}
