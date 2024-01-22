00d80a189fd919b3ac5f3821b2373270bb0cad39
Bayomi Aremo <8955yomi@gmail.com>
2024-01-21 22:25:48 -0700

    contact page almost done ✔

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Home from "./Home";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 // import About from "./components/About/About";
 // import Services__Short from "./components/Services/Services__Short";
 // import Portfolio from "./components/Portfolio/Portfolio";
@@ -26,6 +27,7 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/meet-the-team" element = {<Meettheteam />} />
             <Route path="/about" element={<About />} />
+            <Route path="/contact" element ={<Contact/>} />
           </Routes>
 
           <Footer />
