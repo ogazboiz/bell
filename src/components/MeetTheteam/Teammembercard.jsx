4e8ad7168fb55427b936ae34b6a4c151335f9eb4
Bayomi Aremo <8955yomi@gmail.com>
2024-01-26 10:59:11 -0700

    ✔

--- src/components/MeetTheteam/Teammembercard.jsx
+++ src/components/MeetTheteam/Teammembercard.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-
-
+import React from "react";
 
 const Teammembercard = ({ name, role, email, image }) => {
-  return (
-    <div className='cardTeam'>
-      <img src={image} alt={name} className='teamMember' />
-      <h3>{name}</h3>
-      <p>{role}</p>
-      <p><span className='teamMemberMail'>{email}</span></p>
-    </div>
-  );
-}
+	return (
+		<div className="cardTeam">
+			<img src={image} alt={name} className="teamMember" />
+			<h3>{name}</h3>
+			<p className="role">{role}</p>
+			<span className="Blueline"></span>
+			<p className="cardText">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
+				necessitatibus quisquam architecto temporibus et unde sit illum,
+				obcaecati totam eius cum illo ut repellendus modi quod atque distinctio
+				iusto corrupti numquam! Nam quibusdam officia ea!
+			</p>
+			<p>
+				<span className="teamMemberMail">{email}</span>
+			</p>
+		</div>
+	);
+};
 
 export default Teammembercard;
