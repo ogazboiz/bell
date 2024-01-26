0c00dadf2d788d3b0642eedbb617afa468615e4e
Bayomi Aremo <8955yomi@gmail.com>
2024-01-26 11:32:45 -0700

    card done ✔

--- src/components/MeetTheteam/Teammembercard.jsx
+++ src/components/MeetTheteam/Teammembercard.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+import { CgInstagram } from "react-icons/cg";
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 const Teammembercard = ({ name, role, email, image }) => {
 	return (
 		<div className="cardTeam">
@@ -16,6 +20,33 @@ const Teammembercard = ({ name, role, email, image }) => {
 			<p>
 				<span className="teamMemberMail">{email}</span>
 			</p>
+      <ul className="footer__socials">
+            <li>
+            <a href="https://www.facebook.com/profile.php?id=61554162863933" target="_blank">
+          <AiFillFacebook />
+        </a>
+            </li>
+            <li>
+            <a href="https://www.instagram.com/belsoftsystems/" target="_blank">
+          <CgInstagram />
+        </a>
+            </li>
+            <li>
+            <a href="https://twitter.com/BELSOFT_SYSTEMS" target="_blank">
+          <AiFillTwitterSquare />
+        </a>
+            </li>
+            <li>
+            <a href="https://www.linkedin.com/company/100826786/admin/feed/posts/?feedType=following" target="_blank">
+          <AiFillLinkedin />
+        </a>
+            </li>
+            <li>
+            <a href="https://github.com/BELSOFT-SYSTEMS" target="_blank">
+          <AiFillGithub />
+        </a>
+            </li>
+          </ul>
 		</div>
 	);
 };
