df931806408434d0e1f7d45476da68237c529c8e
Bayomi Aremo <8955yomi@gmail.com>
2024-01-27 07:42:57 -0700

    contact page ✔✔

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,10 +6,11 @@ import { AiFillTwitterSquare } from "react-icons/ai";
 import { AiFillFacebook } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
-
-
+import { FaPhone } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
 const Contact = () => {
-  // const [contact, setContact] = useState("");
+	// const [contact, setContact] = useState("");
 
 	// const handleContactChange = (newContact) => {
 	// 	setContact(newContact);
@@ -18,17 +19,27 @@ const Contact = () => {
 		<div className="all-container">
 			<div className="content-container first-div">
 				<div>
-					<h3>Contact Information</h3>
-					<p>
-						when you fill out the form, our team will get back to you with in 24
-						hours.{" "}
-					</p>
-					<p>Tel : +2348068817499, +2347017097447.</p>
-					<p>Email: enquiry@belsoftsystems.com</p>
-					<p>
-						Plot 208 44 Crescent Purple Jucip Und St Setraco/Gishiri, Gwarimpa,
-						Abuja-Nigeria.
-					</p>
+					<div className="contactinfodiv">
+						<p>
+							{" "}
+							<FaPhone className="contacticons" /> Tel : +2348068817499,
+							+2347017097447.
+						</p>
+					</div>
+					<div className="contactinfodiv">
+						<p>
+							{" "}
+							<IoIosMail className="contacticons" /> Email:
+							enquiry@belsoftsystems.com
+						</p>
+					</div>
+					<div className="contactinfodiv">
+						<p>
+							<FaLocationDot className="contacticons" />
+							Plot 208 44 Crescent Purple Jucip Und St Setraco/Gishiri,
+							Gwarimpa, Abuja-Nigeria.
+						</p>
+					</div>
 				</div>
 				<ul className="footer__socials">
 					<li>
@@ -64,7 +75,10 @@ const Contact = () => {
 			</div>
 			<div className="content-container">
 				<h3>Contact Us </h3>
-        <p className="text">you can get in touch with us by filling this form or emailing us directly</p>
+				<p className="text">
+					you can get in touch with us by filling this form or emailing us
+					directly
+				</p>
 				<ContactInput />
 			</div>
 		</div>
